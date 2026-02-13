e464cd58fe70eb18bdc4adab8b262ca9e071754a
xunqianhui <xunqianhui@gmail.com>
2026-02-13 20:23:25 +0800

    fix: 修复 api/index.ts 导入路径

--- service/api/index.ts
+++ service/api/index.ts
@@ -6,13 +6,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import { env } from './src/config/env';
-import { createSupabaseClient } from './src/config/database';
-import { createRedisClient } from './src/config/redis';
-import { requestId } from './src/middlewares/logger.middleware';
-import { errorHandler, notFoundHandler } from './src/middlewares/error.middleware';
-import { rateLimiter } from './src/middlewares/rate-limit.middleware';
-import routes from './src/routes';
+import { env } from '../src/config/env';
+import { createSupabaseClient } from '../src/config/database';
+import { createRedisClient } from '../src/config/redis';
+import { requestId } from '../src/middlewares/logger.middleware';
+import { errorHandler, notFoundHandler } from '../src/middlewares/error.middleware';
+import { rateLimiter } from '../src/middlewares/rate-limit.middleware';
+import routes from '../src/routes';
 
 const app = express();
 
